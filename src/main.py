6e10ba02281dda7b44db40d2ae2d477e68f3aa62
Par-zy <j.davi4242@gmail.com>
2023-06-22 09:53:27 -0300

    Chamei a classe BMH no main

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from ShiftAnd import ShiftAnd
+from BoyerMooreHorspool import BoyerMooreHorspool
 
 if __name__ == "__main__":
 
@@ -12,19 +13,35 @@ if __name__ == "__main__":
             texto += line
             line = file.readline()
 
-        shift_and = ShiftAnd(padrao, texto)
+        print("O algoritmo de casamento de cadeia usado será:\n"
+              "- BMH (Digite 1)\n"
+              "- ShiftAnd (Digite 2)\n")
+        algoritmo_utilizado = int(input("Resposta: "))
 
-        print("O tipo de busca deve ser:\n"
-              "- Exata (Digite 1)\n"
-              "- Aproximada (Digite 2)\n")
+        if algoritmo_utilizado == 1:
+            bmh = BoyerMooreHorspool(padrao)
+            casamentos = bmh.buscar(texto)
 
-        tipo_de_busca = int(input("Resposta: "))
+            if casamentos:
+              print("Casamentos exatos encontrados nas posições:", casamentos)
+            else:
+              print("Nenhum casamento encontrado.")
 
-        if tipo_de_busca == 1:
-            casamentos_exatos = shift_and.casamento_exato()
-            print("Casamentos exatos encontrados:", casamentos_exatos)
-        elif tipo_de_busca == 2:
-            casamentos_aproximados = shift_and.casamento_aproximado()
-            print("Casamentos aproximados encontrados:", casamentos_aproximados)
-        else:
-            raise Exception("A entrada dada pelo usuário não se encaixa nas entradas aceitas pelo sistema!")
+        elif algoritmo_utilizado == 2:
+            
+            shift_and = ShiftAnd(padrao, texto)
+
+            print("O tipo de busca deve ser:\n"
+                  "- Exata (Digite 1)\n"
+                  "- Aproximada (Digite 2)\n")
+
+            tipo_de_busca = int(input("Resposta: "))
+
+            if tipo_de_busca == 1:
+                casamentos_exatos = shift_and.casamento_exato()
+                print("Casamentos exatos encontrados:", casamentos_exatos)
+            elif tipo_de_busca == 2:
+                casamentos_aproximados = shift_and.casamento_aproximado()
+                print("Casamentos aproximados encontrados:", casamentos_aproximados)
+            else:
+                raise Exception("A entrada dada pelo usuário não se encaixa nas entradas aceitas pelo sistema!")
