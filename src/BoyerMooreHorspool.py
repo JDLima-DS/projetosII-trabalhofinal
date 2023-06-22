bf76c9785a64abbb61968bd033ea8e3c9f6abe6a
Par-zy <j.davi4242@gmail.com>
2023-06-22 09:52:15 -0300

    Adicionei a classe BoyerMooreHorspool

--- src/BoyerMooreHorspool.py
+++ src/BoyerMooreHorspool.py
@@ -1,21 +1,47 @@
 class BoyerMooreHorspool:
-    def __init__(self, padrao_nao_processado: str, texto: str) -> None:
-        self.padrao = self.processa_padrao(padrao_nao_processado)
-        self.tamanho_padrao = len(self.padrao)
-        self.texto = texto
+    def __init__(self, padrao):
+        self.padrao = padrao
+        self.tamanho_padrao = len(padrao)
+        self.tabela_deslocamentos = self.preprocessar_padrao()
 
-    def processa_padrao(self, padrao_nao_processado: str) -> dict:
+    def preprocessar_padrao(self):
         """
-        :param padrao_nao_processado: o padrao que se quer identificar no texto
-        :return: um dicionário no qual:
-                - Chave -> {a, z} sendo uma letra do padrao
-                - Valor -> {1, 9} sendo quantos espaços "pular" quando o último caractere no processamento
-                            for a letra da chave
+        Pré-processa o padrão para construir a tabela de deslocamentos.
+        :return: A tabela de deslocamentos.
         """
-        pass
+        tabela_deslocamentos = {}
+        for i in range(self.tamanho_padrao - 1):
+            tabela_deslocamentos[self.padrao[i]] = self.tamanho_padrao - i - 1
+        return tabela_deslocamentos
 
-    def processa_matchs(self) -> list:
+    def buscar(self, texto):
         """
-        :return: uma lista de índices, sendo os índices onde o padrao dá 'match' com o texto
+        Realiza a busca exata do padrão no texto usando o algoritmo Boyer-Moore-Horspool.
+        :param texto: O texto onde será realizada a busca.
+        :return: Uma lista contendo as posições de casamento do padrão no texto.
         """
-        pass
+        tamanho_texto = len(texto)
+        matches = []
+
+        if self.tamanho_padrao > tamanho_texto:
+            return matches
+
+        i = self.tamanho_padrao - 1
+
+        # Loop principal que percorre o texto em busca de casamentos exatos do padrão
+        while i < tamanho_texto:
+            j = self.tamanho_padrao - 1
+            k = i
+
+            # Loop interno para comparar o padrão com o texto a partir da última posição do padrão
+            while j >= 0 and texto[k] == self.padrao[j]:
+                j -= 1
+                k -= 1
+            # Se o loop interno chegou ao fim com j = -1, isso significa que um casamento exato foi encontrado
+            if j == -1:
+                matches.append(k + 1)
+            # Obtém o deslocamento com base no último caractere do texto sendo comparado
+            deslocamento = self.tabela_deslocamentos.get(texto[i], self.tamanho_padrao)
+            i += deslocamento
+
+        return matches
