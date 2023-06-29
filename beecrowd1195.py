22e8b009cf4f87d3b79dce29e8f3b45d53f5a952
moreqon <98001570+moreqon@users.noreply.github.com>
2023-06-28 23:55:07 -0300

    Update da Questão 1195

--- beecrowd1195.py
+++ beecrowd1195.py
@@ -1,20 +1,25 @@
-# Número de Casos
+# Recebe a Quantidade de Casos de Teste
 C = int(input())
 
+# Lista que Armazena as Váriaveis de cada Caso de Teste
 casos = []
 for i in range(C):
+    # Recebe a Quantidade de Elementos da Árvore
     N = int(input())
+    # Recebe a Chave de cada Elemento
     K = list(map(int, input().split()))
-    casos.append([N, K])
+    casos.append([i + 1, N, K])
 
 
+# Classe Nó
 class Node:
-    def _init_(self, value):
+    def __init__(self, value):
         self.left = None
         self.right = None
         self.value = value
 
 
+# Função Inserção de Nós na Árvore Binária de Busca
 def insert(root, value):
     if root is None:
         return Node(value)
@@ -22,49 +27,52 @@ def insert(root, value):
         root.left = insert(root.left, value)
     else:
         root.right = insert(root.right, value)
-
     return root
 
-
-count = 0
+# Execução do Algoritmo para os Casos de Teste
 for caso in casos:
-    count += 1
-    N, K = caso
+    # Recuperação dos Valores de cada Variável
+    count, N, K = caso
 
+    # Criação da Árvore Binária de Busca
     tree = None
     for i in K:
         tree = insert(tree, i)
 
+    # Impressão dos Percursos
+    aux = []
+    print("Case", str(count) + ":")
+
+    # Função Recursiva Pré-Ordem
     def pre_order(node):
         if node is not None:
             aux.append(node.value)
             pre_order(node.left)
             pre_order(node.right)
         return aux
+    print("Pre.:", end=" ")
+    print(str(pre_order(tree)).translate({ord(i): None for i in "[],"}))
 
+    aux.clear()
+    # Função Recursiva Em-Ordem
     def in_order(node):
         if node is not None:
             in_order(node.left)
             aux.append(node.value)
             in_order(node.right)
         return aux
+    print("In..:", end=" ")
+    print(str(in_order(tree)).translate({ord(i): None for i in "[],"}))
 
+    aux.clear()
+    # Função Recursiva Pós-Ordem
     def post_order(node):
         if node is not None:
             post_order(node.left)
             post_order(node.right)
             aux.append(node.value)
         return aux
-
-    print(f"Case {count}:")
-
-    aux = []
-    print("Pre.:", end=" ")
-    print(str(pre_order(tree)).translate({ord(i): None for i in "[],"}))
-    aux = []
-    print("In..:", end=" ")
-    print(str(in_order(tree)).translate({ord(i): None for i in "[],"}))
-    aux = []
     print("Post:", end=" ")
     print(str(post_order(tree)).translate({ord(i): None for i in "[],"}))
+
     print()
