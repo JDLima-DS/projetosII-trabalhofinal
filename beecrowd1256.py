93263031725a00c87906418a6d5a870d02cf5005
moreqon <98001570+moreqon@users.noreply.github.com>
2023-06-29 10:33:30 -0300

    Update da Questão 1256

--- beecrowd1256.py
+++ beecrowd1256.py
@@ -1,32 +1,31 @@
-# Quantidade de Casos de Teste
+# Recebe a Quantidade de Casos de Teste
 N = int(input())
 
-casos_de_teste = []
-for _ in range(N):
+# Lista que Armazena as Variaveis de cada Caso de Teste
+casos = []
+for i in range(N):
     # Quantidade de Endereços-Base na Tabela e Quantidade de Chaves
     M, C = map(int, input().split())
-
     # Chaves a serem Armazenadas
     E = list(map(int, input().split()))
+    casos.append([i + 1, M, E])
 
-    # Guarda as Variáveis dos Casos de Teste
-    casos_de_teste.append([M, E])
+# Execucao do Algoritmo para os Casos de Teste
+for caso in casos:
+    # Recuperacao dos Valores de cada Variavel
+    count, M, E = caso
 
-count = 0
-for caso in casos_de_teste:
-    count += 1
-    M, E = caso
-
-    # Cria a Tabela
+    # Cria a Tabela Hash de Listas Vazias para o Encadeamento Exterior
     tabela = []
     for i in range(0, M):
         tabela.append([None])
 
-    # Insere os Elementos
+    # Insere os Elementos na Tabela Hash
     for i in E:
+        # Funcao Hash com .insert(-1, i) para o Encadeamento Exterior
         tabela[i % M].insert(-1, i)
 
-    # Imprime a Tabela
+    # Imprime a Tabela conforme o Beecrowd demonstra na Questao
     for i in range(0, M):
         print(i, "->", end=" ")
         for j in tabela[i]:
@@ -35,5 +34,6 @@ for caso in casos_de_teste:
                 continue
             print(j, "->", end=" ")
 
+    # Imprime uma Linha em Branco entre dois Conjuntos de Saida
     if count < N:
         print()
